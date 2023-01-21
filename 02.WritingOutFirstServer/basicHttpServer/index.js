968f332e2167e5d441dc6d9603f970dd4658eafa
Apple <apple@Apples-MacBook-Air.local>
2023-01-21 20:24:36 +0530

    update 2nd lecture module

--- 02.WritingOutFirstServer/basicHttpServer/index.js
+++ 02.WritingOutFirstServer/basicHttpServer/index.js
@@ -4,15 +4,21 @@ const fs = require('fs');
 
 function requestHandler(req,res) {
     console.log(req.url);
-    res.writeHead(200,{'content-type':"text/html"})
-    fs.readFile("./index.html",function(err,data){
+    res.writeHead(200,{'content-type':"text/html"});
+    let targetFile;
+    switch(req.url){
+        case '/' : targetFile = './index.html'; break;
+        case '/profile' : targetFile = './profile.html'; break;
+        default : targetFile = './404.html'; break;
+    }
+    fs.readFile(targetFile,function(err,data){
         if(err){
             console.log("Error",err);
             res.end("<h1>Error</h1>");
             return;
         }
         return res.end(data);
-    })
+    });
 }
 
 const server = http.createServer(requestHandler);
